c0bd5a41022156fb9eb1d329fb17486a5a0620e7
McKeoghN <mckeoghn@gmail.com>
2024-08-02 20:09:30 +0100

    Added exception handling for user input

--- Main.py
+++ Main.py
@@ -2,15 +2,17 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from pprint import pprint
 import numpy as np
+import time
 import Constants
 
-spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=Constants.cid,
-                                               client_secret=Constants.secret,
-                                               redirect_uri=Constants.redirect_uri,
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=Constants.cid, \
+                                               client_secret=Constants.secret, \
+                                               redirect_uri=Constants.redirect_uri, \
                                                scope=Constants.scope))
 
 
 def create_new_playlist(tracks, playlist_name):
+    # Need to change it so my Spotify username isn't public
     new_playlist = spotify.user_playlist_create('mckeoghn', 'Slaylistified ' + playlist_name)
     playlist_id = new_playlist['id']
     spotify.playlist_add_items(playlist_id, tracks)
@@ -47,16 +49,30 @@ def get_user_playlists():  # Get the users 100 newest playlists
 
 def main():
     p_names = get_user_playlists()
-    for i in range(len(p_names)):
+    numPlaylists = len(p_names)
+    for i in range(numPlaylists):
         print(i+1, p_names[i][0])
     print('\n')
-    playlist_input = int(input('What playlist do you want Slaylistified?'))
-    s_names = get_playlist_bpms(p_names[playlist_input-1][1])
+
+    # Error handling for non integer inputs:
+    try:  
+        # User input to select a playlist:
+        playlist_input = int(input('What playlist do you want Slaylistified?'))  
+    except ValueError:
+        print("\nIncorrect input, try again.\n")
+        time.sleep(0.5)
+        main()
+
+    # Error handling for invalid integer inputs:
+    if playlist_input > numPlaylists or playlist_input <= 0:  
+        print("\nIncorrect input, try again.\n")
+        time.sleep(0.5)
+        main()
+    
     s_names = sorted(s_names, key=lambda x: x[2])
     pprint(s_names)
     print('\n')
     np_s_names = np.array(s_names)
-
     create_new_playlist(np_s_names[:,1], p_names[playlist_input-1][0])
 
 
